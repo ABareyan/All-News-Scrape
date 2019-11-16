fab4b77399cfba6adfb56819e97690897ff8c894
Arman Bareyan <abareyan@gmail.com>
2019-11-16 00:31:19 -0700

    Add mongoose

---
+++ server.js
@@ -0,0 +1,40 @@
+// Exoress - Router
+var express = require("express");
+var mongoose = require("mongoose");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+
+var app =  express();
+
+var router = express.Router();
+
+// Routes
+require("./route/routes")(router);
+
+app.use(express.static(__dirname + "/public"));
+
+// Handlebars
+app.engine("handlebars", expressHandlebars({defaultLayout: "main"}))
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(router);
+
+//Mongoose
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(db, function(error) {
+    if (error) {
+        throw error;
+    } else {
+        console.log("mongoose connection success!");
+        
+    }
+})
+
+app.listen(PORT, function() {
+    console.log("Listening on port" + PORT);
+});
